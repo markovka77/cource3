a79e559681fa098909d87cb60e0aa4b1da5e1b22
Mark <voronkov1mark@gmail.com>
2023-09-15 22:56:09 +0300

    done

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -4,21 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
-import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repositories.FacultyRepository;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
+import java.util.stream.Stream;
 
 @Service
 public class FacultyService {
     Logger logger = LoggerFactory.getLogger(FacultyService.class);
-    private FacultyRepository facultyRepository;
+    private final FacultyRepository facultyRepository;
+
 
     public FacultyService(FacultyRepository facultyRepository) {
         this.facultyRepository = facultyRepository;
+
     }
 
     public Faculty createFaculty(Faculty faculty) {
@@ -46,20 +45,34 @@ public class FacultyService {
         return facultyRepository.findAll();
     }
 
-    public  Collection<Faculty> sortByColor(String color) {
+    public Collection<Faculty> sortByColor(String color) {
         logger.debug("sortByColor is running");
         return facultyRepository.findFacultyByColor(color);
     }
-    public Collection<Faculty> sortByName(String name){
+
+    public Collection<Faculty> sortByName(String name) {
         logger.debug("sortByName is running");
         return facultyRepository.findByNameIgnoreCase(name);
     }
-    public Collection<Faculty>getStudentsOfFaculty(String name){
+
+    public Collection<Faculty> getStudentsOfFaculty(String name) {
         logger.debug("getStudentsOfFaculty is running");
         return facultyRepository.findFacultiesByStudents(name);
     }
 
+    public String longestNameOfFaculty() {
+        return facultyRepository.findAll().stream()
+                .max(Comparator.comparing(f->f.getName().length()))
+                .map(Faculty::getName).toString();
+    }
 
-// 2 и 3 доделать
+    public int sum(){
+
+        return Stream.iterate(1, a -> a +1)
+                .limit(1_000_000)
+                .parallel()
+                .reduce(0, Integer::sum);
+    }
+
 
 }
